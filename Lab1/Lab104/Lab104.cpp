1a9993af6030944a793bd42f69417581400dc692
Alld4y <mhek33529@gmail.com>
2025-12-06 20:57:40 +0700

    Lab1.4 CLO3 fixed

--- Lab1/Lab104/Lab104.cpp
+++ Lab1/Lab104/Lab104.cpp
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
-int GetSet(int []);
+int GetSet(int data[]);
 
 int main() {
-    int *data, num;
+    int *data;
+    int num;
 
-    num = GetSet((int*)&data); 
+    num = GetSet((int*)&data); // fill data
 
     for (int i = 0; i < num; i++) {
         printf("[%d]: %d\n", i, data[i]);
     }
 
-    delete[] data;
+    delete[] data; // free memory
+
     return 0;
 }
 
 int GetSet(int data[]) {
-    int **p = (int**)data;
+    int **p = (int**)data; 
     int num;
-    scanf("%d", &num);
 
-    *p = new int[num];
+    scanf("%d", &num);      // read size
+    *p = new int[num];       // allocate array
 
     for (int i = 0; i < num; i++) {
-        scanf("%d", &((*p)[i]));
+        scanf("%d", &((*p)[i])); // read elements
     }
+
     return num;
 }
