2bf990f9a24b14742b0d2bb42104808f6f4b8ec5
Alld4y <mhek33529@gmail.com>
2025-12-06 15:10:53 +0700

    lab1

--- Lab1/Lab101/Lab101.c
+++ Lab1/Lab101/Lab101.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+int main () {
+    int n = 0;
+    if(scanf("%d",&n)!=1){
+        return 1;
+    }
+    printf("%d",n);
+    for(int i = 1 ; i <=n ; i++){
+        printf("[%d] Hello world",i)
+    }
+    return 0;
+}
