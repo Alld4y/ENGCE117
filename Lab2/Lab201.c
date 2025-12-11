c28d3a5521a0496239a0c342bcdb6b1fbf1f3655
Alld4y <mhek33529@gmail.com>
2025-12-11 21:16:07 +0700

    Lab2.1 first commit

---
+++ Lab2/Lab201.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void reverse(char str1[], char str2[]);
+
+int main () {
+    char text[50] = "I Love You";
+    char out[50];
+    reverse(text, out);
+    printf("%s",out);
+    return 0;
+}
+
+void reverse(char str1[], char str2[]) {
+    int len = 0;
+
+    while(str1[len] != '\0') {
+        len++;
+    }
+
+    for(int i = 0 ; i < len ; i++) {
+        str2[i] = str1[len-1-i];
+    }
+    str2[len] = '\0';
+}
