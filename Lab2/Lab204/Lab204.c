a041f9605a88e07c61ea631bc6521ce274d352b2
Alld4y <mhek33529@gmail.com>
2025-12-11 23:20:09 +0700

    Lab2.3 2.4 insert comment

--- Lab2/Lab204/Lab204.c
+++ Lab2/Lab204/Lab204.c
@@ -4,32 +4,34 @@ void explode(char str1[], char splitter, char str[][10], int *count);
 
 int main () {
     char out[20][10];
-    int num;
     int count = 0;
-    explode("I/Love/You", '/', out, &count );
+
+    explode("I/Love/You", '/', out, &count);
+
     for(int i = 0 ; i < count ; i++){
-        printf("str2[%d] = %s\n",i, out[i]);
+        printf("str2[%d] = %s\n", i, out[i]);  // print each token
     }
-    printf("count = %d",count);
+
+    printf("count = %d", count);  // print number of tokens
     return 0;
 }
 
 void explode(char str1[], char splitter, char str[][10], int *count) {
-    int i = 0;
-    int j = 0;
+    int i = 0;  // index for str1
+    int j = 0;  // index for str
 
     if(str1[0] != '\0')
-        (*count) = 1;
-    while(str1[i] != '\0') {
-        if(str1[i] == splitter) {
-            str[(*count)-1][j] = '\0';
+        (*count) = 1; 
+
+    while(str1[i] != '\0') {  
+        if(str1[i] == splitter) {  // if splitter found
+            str[(*count)-1][j] = '\0';  // end current
             (*count) += 1;
             j = 0;
         } else {
-            str[(*count)-1][j] = str1[i];
-            j += 1;
+            str[(*count)-1][j] = str1[i];  // copy character
+            j += 1;  // move to next position
         }
-        i += 1;
-    } 
+        i += 1;  // move to next character in str1
+    }
 }
-
