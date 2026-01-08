61aec1af3dd3e81c41f04bcb49b4c26f40eaeef2
Alld4y <mhek33529@gmail.com>
2026-01-08 23:10:31 +0700

    Lab4.1 first commit

--- Lab3/Lab301/Lab301.c
+++ Lab3/Lab301/Lab301.c
@@ -9,20 +9,29 @@ typedef struct {
 
 void upgrade(student child);
 
-int main() {
+int main(void) {
     student aBoy;
+
+    /* Initialize student data */
     aBoy.sex = 'M';
     aBoy.gpa = 3.00;
+
+    /* Pass structure by value */
     upgrade(aBoy);
+
+    /* GPA remains unchanged because a copy was modified */
     printf("%.2f\n", aBoy.gpa);
-    printf("upgrading GPA is not changed because struct is passed by value.\n");
+    printf("Upgrading GPA is not changed because struct is passed by value.\n");
+
     return 0;
 }
 
 void upgrade(student child) {
-    if(child.sex == 'M') {
-        child.gpa += 0.1 * child.gpa;
-    } else if (child.sex == 'F') {
-        child.gpa += 0.2 * child.gpa;
+    /* Modify GPA based on gender (local copy only) */
+    if (child.sex == 'M') {
+        child.gpa = child.gpa + (0.1 * child.gpa);
+    }
+    else if (child.sex == 'F') {
+        child.gpa = child.gpa + (0.2 * child.gpa);
     }
 }
